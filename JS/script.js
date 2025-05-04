432789a4e1b5b65bc5880ceee9eba7be43ca2af4
Awab Abdelbagi <awaababdelbagi@gmail.com>
2025-05-04 09:35:59 +0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,1 @@
-const catogrey = document.querySelectorAll(".catogrey span");
-let catogreySelect;
 
-catogrey.forEach((span) => {
-    span.onclick = () => {
-        catogrey.forEach((el) => {
-            el.classList.remove("check");
-        });
-        span.classList.add("check");
-    }
-});
